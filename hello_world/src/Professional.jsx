f173b722fa6606ac17050ffee6ccd76b924188fd
Kebitsamang Raleche <raleche.kn@gmail.com>
2023-11-15 22:15:32 +0200

    React Assigment 2 (JSX)

---
+++ hello_world/src/Professional.jsx
@@ -0,0 +1,18 @@
+
+function Professional() { 
+  
+  return (
+    <>
+      <h1>Kebitsamang Raleche</h1>
+      <p>I studied a four year degree in BSc. Computer Science in 2013 at National Univesity of Lesotho.</p>
+      <p>I also completed the following professional certificates:</p>
+      <ol>
+        <li>Digital Transformation: From AI and IoT to Cloud, Blockchain, and Cybersecurity</li>
+        <li>Data Science</li>
+        <li>Business and Systems Analysis</li>
+      </ol>
+    </>
+  )
+}
+
+export default Professional
